1e4fa7c1f6f0772db937ad6fa34872c6747ea9ce
Mahender <mahender.lfl95@gmail.com>
2019-09-30 12:32:37 -0500

    errors rectified

--- java.js
+++ java.js
@@ -2,7 +2,7 @@ function calc()
 {
     let box1=  parseFloat(document.getElementById("box1").value);
     let box2=  parseFloat(document.getElementById("box2").value);
-    let max=math.max(box1,box2);
+    let max=Math.max(box1,box2);
     document.getElementById("final").innerHTML=max;
 }
 document.querySelector('#cal').addEventListener('click',calc);
